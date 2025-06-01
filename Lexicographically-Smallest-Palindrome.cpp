6fa3c74cb4e8c1a9daf23f9c8cd78dc27dcd5d8b
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-06-01 17:09:30 +0530

    [2697] [Time Beats: 37.78%] [Memory Beats: 27.35%] - LeetPush

---
+++ Lexicographically-Smallest-Palindrome.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int n = s.length();
+        int i = 0;
+        int j = n - 1;
+
+        while(i <= j){
+            if(s[i] < s[j]){
+                s[j] = s[i];
+            }else s[i] = s[j];
+            i++;
+            j--;
+        }
+        return s;
+    }
+};

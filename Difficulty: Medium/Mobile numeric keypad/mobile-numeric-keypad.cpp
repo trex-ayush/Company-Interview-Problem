91420531f136ecf88b334600724fefcaa4183624
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-06-27 18:22:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Mobile numeric keypad/mobile-numeric-keypad.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    int dp[4][4][16];
+
+    int solve(int i, int j, int n) {
+        if (i < 0 || i >= 4 || j < 0 || j >= 3 || (i == 3 && (j == 0 || j == 2))) {
+            return 0;
+        }
+        // base case: sequence length 1 → only the current key
+        if (n == 1) return 1;
+        
+        if (dp[i][j][n] != -1) return dp[i][j][n];
+        
+        int result = 0;
+        
+        result += solve(i, j, n - 1) +
+                  solve(i - 1, j, n - 1) + 
+                  solve(i, j - 1, n - 1) + 
+                  solve(i, j + 1, n - 1) + 
+                  solve(i + 1, j, n - 1);
+                  
+        return dp[i][j][n] = result;
+    }
+
+    int getCount(int n) {
+        int count = 0;
+        memset(dp, -1, sizeof(dp));
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                count += solve(i, j, n);
+            }
+        }
+        return count;
+    }
+};

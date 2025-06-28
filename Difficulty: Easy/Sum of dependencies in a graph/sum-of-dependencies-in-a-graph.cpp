f1542171647808be2b9ca95f790ae38334e08b7f
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-06-28 17:07:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of dependencies in a graph/sum-of-dependencies-in-a-graph.cpp
@@ -0,0 +1,9 @@
+//Back-end complete function Template for C++
+
+class Solution {
+  public:
+    // Function to calculate the sum of dependencies.
+    int sumOfDependencies(int V, vector<vector<int>> &edges) {
+        return edges.size();
+    }
+};

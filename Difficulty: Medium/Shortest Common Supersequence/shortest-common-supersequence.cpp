5a8330ad4e7d061d565546c07b358cd3cca010c7
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-07-09 21:26:55 +0530

    3 Approaches shortest-common-supersequence.cpp

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -1,5 +1,51 @@
 class Solution {
   public:
+    int getLCS(string& text1, string& text2){
+        int n = text1.size();
+        int m = text2.size();
+        
+        vector<vector<int>> t(n+1, vector<int>(m+1, 0));
+        
+        for(int i = 1; i < n+1; i++){
+            for(int j = 1; j < m+1; j++){
+                if(text1[i-1] == text2[j-1]) t[i][j] = 1 + t[i-1][j-1];
+                else t[i][j] = max(t[i-1][j], t[i][j-1]);
+            }
+        }
+        return t[n][m];
+    }
+    int shortestCommonSupersequence(string &s, string &s2) {
+        int lcs = getLCS(s, s2);
+        
+        return s.size() + s2.size() - lcs;
+    }
+    
+    /* Bottom Up */
+    int solve(string &s, string &t){
+        int n = s.size();
+        int m = t.size();
+        
+        vector<vector<int>> dp(n+1, vector<int>(m+1));
+        
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < m+1; j++){
+                if(i == 0 || j == 0){
+                    dp[i][j] = i + j;
+                }else if(s[i-1] == t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }else {
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    int shortestCommonSupersequence(string &s, string &s2) {
+        return solve(s,s2);
+    }
+    
+    
+    /* Recursion + Memo */
     int t[501][501];
     int solve(string& s, string&s2, int n, int m){
         if(n == 0 || m == 0) return n + m;

795c6bb84984961fce55e9dec3ca31d114049df6
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-07-09 17:38:45 +0530

    [583] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Delete-Operation-for-Two-Strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int getLCS(string& word1, string& word2, int i, int j, vector<vector<int>>& dp){
+        if(i == word1.size() || j == word2.size()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(word1[i] == word2[j]) return dp[i][j] = 1 + getLCS(word1, word2, i + 1, j + 1, dp);
+        else return dp[i][j] = max(getLCS(word1, word2, i + 1, j, dp), getLCS(word1, word2, i, j + 1, dp));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+
+        int lcs = getLCS(word1, word2, 0, 0, dp);
+
+
+        return n + m - 2*lcs;
+    }
+};

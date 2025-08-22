e7e7fb6ebd0cc9b5fed07a9679e10d3476684329
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-08-23 00:32:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -1,48 +1,30 @@
 class Solution {
   public:
-     int median(vector<vector<int>> &mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        int count;
-        int l=INT_MAX;
-        int h=INT_MIN;
-        for(int i=0;i<n;i++)
-        {
-            l=min(l,mat[i][0]);
+    int findSmallerNumber(vector<vector<int>> &mat, int& mid) {
+        int count = 0;
+        for (int i = 0; i < mat.size(); i++) {
+            count += upper_bound(mat[i].begin(), mat[i].end(), mid) - mat[i].begin();
         }
-        for(int i=0;i<n;i++)
-        {
-            h=max(h,mat[i][m-1]);
+        return count;
+    }
+
+    int median(vector<vector<int>> &mat) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        int totalElement = n*m;
+        int medianIndex = totalElement/2;
+        
+        int st = 1;
+        int en = 2000;
+        
+        while(st <= en){
+            int mid = st + (en - st)/2;
+            int elementLessThanMid = findSmallerNumber(mat, mid);
+            
+            if(elementLessThanMid <= medianIndex) st = mid + 1;
+            else en = mid - 1;
         }
-        int ans=0;
-        while(l<=h)
-        {
-            int mid=(l+h)/2;
-            count=0;
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<m;j++)
-                {
-                    if(mat[i][j]<=mid)
-                    {
-                        count++;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-            }
-            if(n*m-count<=count)
-            {
-                ans=mid;
-                h=mid-1;
-            }
-            else
-            {
-                l=mid+1;
-            }
-        }
-        return ans;
+        return st;
     }
 };

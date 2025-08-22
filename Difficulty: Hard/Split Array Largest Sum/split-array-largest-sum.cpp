ae4d4a1dea613200de6c5d56324d2cc4d728b98b
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-08-23 01:22:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
+++ Difficulty: Hard/Split Array Largest Sum/split-array-largest-sum.cpp
@@ -1,43 +1,44 @@
 class Solution {
   public:
-    bool isPossible(vector<int>& arr, int k, int& mid){
-        int n = arr.size();
-        int split = 1;
-        int sum = 0;
+    bool isPossible(vector<int>& arr, int k, int pages){
+        int studentCount = 1;
+        int totalPages = 0;
         
-        for(int i = 0; i < n; i++){
-            if (arr[i] > mid) return false;
-            if (sum + arr[i] > mid) {
-                split++;
-                sum = arr[i];
-                
-                if(split > k) return false;
-            } else {
-                sum += arr[i];
+        for(int i = 0; i < arr.size(); i++){
+            
+            if(totalPages + arr[i] <= pages){
+                totalPages += arr[i];
+            }else{
+                studentCount++;
+                totalPages = arr[i];
             }
+            if(studentCount > k || arr[i] > pages) return false;
         }
-        return split <= k;
+        return true;
     }
     int splitArray(vector<int>& arr, int k) {
         int n = arr.size();
-        int maxElement = -1;
-        int totalSum = 0;
         
-        for(int i = 0; i < n; i++){
-            totalSum += arr[i];
-            if(arr[i] > maxElement) maxElement = arr[i];
+        int totalPages = 0;
+        int st = 0;
+        
+        for(auto& a : arr) {
+            st = max(a, st);
+            totalPages += a;
         }
         
-        int st = maxElement;
-        int en = totalSum;
+        int en = totalPages;
         int ans = -1;
         
         while(st <= en){
             int mid = st + (en - st)/2;
+            
             if(isPossible(arr, k, mid)){
                 ans = mid;
                 en = mid - 1;
-            }else st = mid + 1;
+            }else{
+                st = mid + 1;
+            }
         }
         
         return ans;

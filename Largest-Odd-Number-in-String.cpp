da09103a97edda54596f58a43467df22c2c833e9
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-08-27 22:34:40 +0530

    [1903] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Largest-Odd-Number-in-String.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // Time Complxity  : O(n)
+    // SPace Complxity : O(1)
+    string largestOddNumber(string s) {
+        int n = s.length();
+        
+        for(int i = n - 1; i >= 0; i--){
+            char ch = s[i];
+            int num = ch - '0';
+            
+            if(num % 2 != 0){
+                return s.substr(0, i+1);
+            }
+        }
+        return "";
+    }
+};

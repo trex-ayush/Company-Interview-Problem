639eca62b01220d62f6e111b6141fd29acd804e8
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-08-30 17:05:07 +0530

    [13] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Roman-to-Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> mpp = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        }; 
+        int sum = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            char ch = s[i];
+            sum += mpp[ch];
+            if(i > 0 && mpp[s[i-1]] < mpp[ch]) sum -= 2*mpp[s[i-1]];
+        }
+        
+        return sum;
+    }
+};

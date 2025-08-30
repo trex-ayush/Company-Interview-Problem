224e82868e095841561eecc3e43cb2b395e76796
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-08-30 12:56:33 +0530

    [205] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ Isomorphic-Strings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        
+        if(n != m) return false;
+        
+        unordered_map<char,char> sTOt;
+        unordered_map<char,char> tTOs;
+        
+        for(int i = 0; i < n; i++){
+            int ch1 = s[i];
+            int ch2 = t[i];
+            
+            if(sTOt.find(ch1) != sTOt.end() && sTOt[ch1] != ch2) return false;
+            if(tTOs.find(ch2) != tTOs.end() && tTOs[ch2] != ch1) return false;
+            
+            sTOt[ch1] = ch2;
+            tTOs[ch2] = ch1;
+        }
+        
+        return true;
+    }
+};

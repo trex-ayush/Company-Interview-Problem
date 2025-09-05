62376085ec110aed2190964c14b82fe1504c5dfc
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-06 03:18:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Combination Sum II/combination-sum-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(vector<int>& arr, int k, int idx, vector<int>& temp, vector<vector<int>>& res) {
+        if (k == 0) {
+            res.push_back(temp);
+            return;
+        }
+        if (idx >= arr.size() || k < 0) return;
+        
+        int nextIdx = idx + 1;
+        while (nextIdx < arr.size() && arr[nextIdx] == arr[idx]) nextIdx++;
+        // skip
+        solve(arr, k, nextIdx, temp, res);
+        // take
+        temp.push_back(arr[idx]);
+        solve(arr, k - arr[idx], idx + 1, temp, res);
+        temp.pop_back();
+    }
+
+    vector<vector<int>> uniqueCombinations(vector<int>& arr, int target) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> res;
+        vector<int> temp;
+        solve(arr, target, 0, temp, res);
+        return res;
+    }
+};

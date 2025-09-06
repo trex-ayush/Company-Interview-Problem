b23adb3a01a6832b748957a816f6d21a877467b6
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-06 18:29:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word Search/word-search.cpp
@@ -0,0 +1,36 @@
+class Solution {
+  public:
+    vector<vector<int>> dirs = {{1,0}, {-1, 0}, {0,1}, {0, -1}};
+    bool solve(vector<vector<char>>& mat, string& word, int i, int j, int wordIdx){
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        if(i >= n || i < 0 || j >= m || j < 0 || mat[i][j] != word[wordIdx] || mat[i][j] == '*') return false;
+        if(wordIdx == word.length() - 1) return true;
+        
+        char temp = mat[i][j];
+        mat[i][j] = '*';
+        
+        for(auto& dir : dirs){
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if(solve(mat, word, x, y, wordIdx+1)) return true;
+        }
+        
+        mat[i][j] = temp;
+        return false;
+    }
+    bool isWordExist(vector<vector<char>>& mat, string& word) {
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(word[0] == mat[i][j]){
+                    if(solve(mat, word, i, j, 0)) return true;
+                }
+            }
+        }
+        return false;
+    }
+};

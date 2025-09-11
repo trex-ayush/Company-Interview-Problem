a9631b955bdcf938fd340a782e6d729666abc28f
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-12 01:29:42 +0530

    [1248] [Time Beats: 8.38%] [Memory Beats: 15.83%] - LeetPush

---
+++ Count-Number-of-Nice-Subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    // even number to 0 
+    // odd number to 1
+    // count subaaray with sum k
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        int sum = 0;
+        unordered_map<int,int> mpp;
+        mpp[0] = 1;
+        int i = 0;
+
+        while(i < n){
+            sum += (nums[i]%2 == 0) ? 0 : 1;
+            if(mpp.find(sum - k) != mpp.end()){
+                count += mpp[sum - k];
+            } 
+            mpp[sum]++;
+            i++;
+        }
+
+        return count;
+    }
+};

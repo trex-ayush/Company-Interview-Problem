1bf1da768aae61986dd3a5759ea4dac8677e8ba1
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-12 20:56:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Replace elements by its rank in the array/replace-elements-by-its-rank-in-the-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    vector<int> replaceWithRank(vector<int> &arr, int n) {
+        vector<int> ans(n);
+        unordered_map<int, pair<int, int>> mpp;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minh;
+
+        for (int i = 0; i < arr.size(); i++) {
+            minh.push({arr[i], i});
+        }
+
+        int rank = 1;
+        while (!minh.empty()) {
+            int value = minh.top().first;
+            int index = minh.top().second;
+
+            if (mpp.find(value) == mpp.end()) {
+                mpp[value] = {rank, index};
+                rank++;
+            }
+
+            ans[index] = mpp[value].first;
+            minh.pop();
+        }
+
+        return ans;
+    }
+};

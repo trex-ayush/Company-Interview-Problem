42fc20f2eb0c005af20e789cb2a0c14147690be8
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-13 21:34:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
+++ Difficulty: Medium/Ceil in BST/ceil-in-bst.cpp
@@ -1,33 +1,18 @@
-/* struct Node {
-    int data;
-    struct Node* left;
-    struct Node* right;
-};  */
-
-// 10 5 11 4 7 N N N N N 8
-// 6
-// 3 1 4 N 2 N N N N
-// 15
-void solve(Node* root, int k, int& result){
-    if(!root) return;
-    
-    if(root->data == k){
-        result = k;
-        return;
+class Solution {
+  public:
+    int findCeil(Node* root, int x) {
+        if(!root) return -1;
+        int result = -1;
+        
+        Node* temp = root;
+        
+        while(temp){
+            if(temp->data == x) return x;
+            else if(temp->data > x){
+                result = temp->data;
+                temp = temp->left;
+            }else temp = temp->right;
+        }
+        return result;
     }
-    if(root->data > k && root->data < result){
-        result = root->data;
-    }
-    solve(root->left, k, result);
-    solve(root->right, k, result);
-}
-
-int findCeil(Node* root, int k) {
-    if(!root) return -1;
-    
-    int result = INT_MAX;
-    
-    solve(root, k, result);
-    
-    return (result == INT_MAX) ? -1 : result;
-}
+};

658fc88305f3cef190a4970f41f8a778d1b47272
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-13 11:21:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top K Frequent in Array/top-k-frequent-in-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    vector<int> topKFrequent(vector<int> &arr, int k) {
+        int n = arr.size();
+        unordered_map<int, int> mpp;
+        
+        for(auto& a : arr) mpp[a]++;
+    
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+        for(auto& mp : mpp){
+            pq.push({mp.second, mp.first});
+            if(pq.size() > k) pq.pop();
+        }
+        vector<int> result;
+        while (!pq.empty()) {
+            result.push_back(pq.top().second);
+            pq.pop();
+        }
+        
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};

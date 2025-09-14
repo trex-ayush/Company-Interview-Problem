f04dd129b3c084af3003bfcce47d9ac477c838e7
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-14 11:57:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Gas Station/gas-station.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int startStation(vector<int> &gas, vector<int> &cost) {
+        int n = gas.size();
+        
+        int totalKamai = accumulate(gas.begin(), gas.end(), 0);
+        int totalKarch = accumulate(cost.begin(), cost.end(), 0);
+        
+        if(totalKamai < totalKarch) return -1;
+        
+        int total = 0;
+        int resultIdx = 0;
+        for(int i = 0; i < n; i++){
+            total += gas[i] - cost[i];
+            if(total < 0) {
+                total = 0;
+                resultIdx = i + 1;
+            }
+        }
+        
+        return resultIdx;
+    }
+};

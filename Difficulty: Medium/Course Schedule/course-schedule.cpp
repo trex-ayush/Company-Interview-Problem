b764bb470251d2bf5cc2fa2fdbf289524a21452d
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-17 12:31:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Course Schedule/course-schedule.cpp
+++ Difficulty: Medium/Course Schedule/course-schedule.cpp
@@ -1,41 +1,34 @@
 class Solution {
   public:
-    // T.C: O(n + m)
-    // S.C: O(n + m)
-    vector<int> findOrder(int n, vector<vector<int>> prerequisites) {
+    vector<int> findOrder(int numCourses, vector<vector<int>> prerequisites) {
         unordered_map<int, vector<int>> adj;
-        vector<int> inDegree(n, 0); 
+        vector<int> indegree(numCourses, 0);
         vector<int> result;
         
-        // Building the graph — O(p)
         for(auto& prerequisite : prerequisites){
-            int u = prerequisite[1];
-            int v = prerequisite[0];
-            adj[u].push_back(v);
-            inDegree[v]++;
-        }
-        
-        // Queue initialization — O(n)
-        queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(inDegree[i] == 0) q.push(i);
-        }
-        
-        // Topological Sort — O(n + p)
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            result.push_back(node);
+            int u = prerequisite[0];
+            int v = prerequisite[1];
             
-            // For each neighbor of node — total of O(p) over the algorithm
-            for(auto& v : adj[node]){
-                inDegree[v]--;
-                if(inDegree[v] == 0){
-                    q.push(v);
-                }
+            adj[v].push_back(u);
+            indegree[u]++;
+        }
+        
+        queue<int> q;
+        
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            result.push_back(u);
+            
+            for(auto& v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0) q.push(v);
             }
         }
-        
         return result;
     }
 };

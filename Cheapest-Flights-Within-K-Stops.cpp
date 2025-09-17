492e5ef847b4b5a8a493bf04b66b1a677bec8688
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-18 00:35:04 +0530

    [787] [Time Beats: 100.00%] [Memory Beats: 90.37%] - LeetPush

--- Cheapest-Flights-Within-K-Stops.cpp
+++ Cheapest-Flights-Within-K-Stops.cpp
@@ -1,8 +1,19 @@
 class Solution {
-private:
-    int solve(unordered_map<int,vector<pair<int,int>>>& adj, int src, int dst, int k,vector<int>& distance){
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        unordered_map<int, vector<pair<int, int>>> adj;  // {u, {v, wt}}
+
+        for(auto& flight : flights){
+            int u = flight[0];
+            int v = flight[1];
+            int wt = flight[2];
+
+            adj[u].push_back({v, wt});
+        }
+
         queue<pair<int,int>> q;  // {node , cost};
         q.push({src,0});
+        vector<int> distance(n, INT_MAX);
         distance[src] = 0;
         int step = 0;
 
@@ -26,18 +37,5 @@ private:
             step++;
         }
         return distance[dst] == INT_MAX ? -1 : distance[dst];
-    }
-public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        unordered_map<int,vector<pair<int,int>>> adj;
-        vector<int> distance(n,INT_MAX);
-
-        for(auto& flight : flights){
-            int u = flight[0];
-            int v = flight[1];
-            int c = flight[2];
-            adj[u].push_back({v,c});
-        }
-        return solve(adj,src,dst,k,distance);  
     }
 };

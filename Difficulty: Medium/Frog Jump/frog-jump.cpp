2013390cc9fcc0a5bbf671cf47160506e1272137
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-24 00:27:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -1,22 +1,18 @@
 class Solution {
   public:
-    unordered_map<int, int> t;
-    int solve(vector<int>& height, int idx){
-        if(idx == 0) return 0;
-        
-        if(t.find(idx) != t.end()) return t[idx];
-        
-        int stepOne = INT_MAX, stepTwo = INT_MAX;
-        
-        if(idx - 1 >= 0) {
-            stepOne = solve(height, idx - 1) + abs(height[idx] - height[idx-1]);
-        }
-        if(idx - 2 >= 0) {
-            stepTwo = solve(height, idx - 2) + abs(height[idx] - height[idx-2]);
-        }
-        return t[idx] = min(stepOne, stepTwo);
-    }
     int minCost(vector<int>& height) {
-        return solve(height, height.size() - 1);
+        int n = height.size();
+        int prev1 = 0;
+        int prev2 = 0;
+        
+        for(int i = 1; i < n; i++){
+            int stepOne = prev1 + abs(height[i] - height[i - 1]);
+            int stepTwo = INT_MAX;
+            if(i > 1) stepTwo = prev2 + abs(height[i] - height[i - 2]);
+            prev2 = prev1;
+            prev1 = min(stepOne, stepTwo);;
+        }
+        
+        return prev1;
     }
 };

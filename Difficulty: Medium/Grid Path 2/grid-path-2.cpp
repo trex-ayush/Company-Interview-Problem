736b29a4e211a22fec4fdfb1d904cbf8bad05fe2
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-09-24 12:31:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Grid Path 2/grid-path-2.cpp
+++ Difficulty: Medium/Grid Path 2/grid-path-2.cpp
@@ -1,22 +1,27 @@
 class Solution {
   public:
-    int t[101][101];
     const int mod = 1e9+7;
-    int solve(vector<vector<int>>& mat, int i, int j){
-        if(i >= mat.size() || j >= mat[0].size() || mat[i][j] == 1) return 0;
-        if(i == mat.size() - 1 && j == mat[0].size() - 1) return 1;
-        
-        if(t[i][j] != -1) return t[i][j];
-        
-        int ans = 0;
-        
-        ans = (ans + solve(mat, i + 1, j)) % mod;
-        ans = (ans + solve(mat, i, j + 1)) % mod;
-        
-        return t[i][j] = ans % mod;
-    }
     int totalWays(int n, int m, vector<vector<int>>& mat) {
-        memset(t,-1,sizeof(t));
-        return solve(mat, 0, 0);
+        if (mat[0][0] == 1 || mat[n-1][m-1] == 1) return 0;
+        
+        vector<vector<int>> t(n, vector<int>(m, 0));
+        t[0][0] = 1;
+        for(int j = 1; j < m; j++) {
+            if (mat[0][j] == 0) t[0][j] = t[0][j - 1];
+            else t[0][j] = 0;
+        }
+        
+        for(int i = 1; i < n; i++) {
+            if (mat[i][0] == 0) t[i][0] = t[i - 1][0];
+            else t[i][0] = 0;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(mat[i][j] != 1) t[i][j] = (t[i][j-1] + t[i-1][j]) % mod;
+            }
+        }
+
+        return t[n-1][m-1];
     }
 };

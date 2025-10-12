9f8c77993e501f79676880209789fff92a21be04
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-10-12 22:42:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Stock Buy and Sell – Max K Transactions Allowed/stock-buy-and-sell-max-k-transactions-allowed.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int maxProfit(vector<int>& prices, int k) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2*k+1, vector<int>(2, 0)));
+
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int maxAllowed = 1; maxAllowed < 2*k+1; maxAllowed++) {
+                for (int canBuy = 0; canBuy <= 1; canBuy++) {
+                    int profit = 0;
+                    if (canBuy) {
+                        int buy = -prices[idx] + dp[idx + 1][maxAllowed - 1][0];
+                        int skip = dp[idx + 1][maxAllowed][1];
+                        profit = max(buy, skip);
+                    } else {
+                        int sell = prices[idx] + dp[idx + 1][maxAllowed - 1][1];
+                        int skip = dp[idx + 1][maxAllowed][0];
+                        profit = max(sell, skip);
+                    }
+                    dp[idx][maxAllowed][canBuy] = profit;
+                }
+            }
+        }
+        return dp[0][2*k][1];
+    }
+};

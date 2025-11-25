979d89e9147575e5542e3123195688cc3190da2e
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-11-25 19:30:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& arr) {
+        int n = arr.size();
+        if(n % 2 == 0) return 0;
+        
+        int x = 0;
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                x ^= arr[i];
+            }
+        }
+        
+        return x;
+    }
+};

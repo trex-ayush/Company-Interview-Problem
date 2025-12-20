553ce557930acf9cd58b4bf383cd8b0bc17fd273
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2025-12-20 22:38:04 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 59.25%] - LeetPush

--- Maximum-Product-Subarray.cpp
+++ Maximum-Product-Subarray.cpp
@@ -1,22 +1,30 @@
-// Company tags: Arcesium, DE Shaw, Google, Goldman Sachs, J.P. Morgan, LinkedIn, MakeMyTrip, Nvidia, Oracle, PayPal, ServiceNow, TCS, TikTok, Wayfair
-class Solution {
-public:
-    // Time Complexity  : O(n)
-    // Space Complexity : O(1)
-    int maxProduct(vector<int>& nums) {
-        int n = nums.size();
-        int pref = 1;
-        int suff = 1;
-        int result = INT_MIN;
-
-        for(int i = 0; i < n; i++){
-            if(pref == 0) pref = 1;
-            if(suff == 0) suff = 1;
-            pref *= nums[i];
-            suff *= nums[n-i-1];
-            
-            result = max({result, pref, suff});
-        }
-        return result;
-    }
-};
+1// Company tags: Arcesium, DE Shaw, Google, Goldman Sachs, J.P. Morgan, LinkedIn, MakeMyTrip, Nvidia, Oracle, PayPal, ServiceNow, TCS, TikTok, Wayfair
+2class Solution {
+3public:
+4    // Time Complexity  : O(n)
+5    // Space Complexity : O(1)
+6
+7    // At each step, the maximum product can be the current element, 
+8    // the previous maximum product times the current element, or 
+9    // the previous minimum product times the current element (in case of two negatives).
+10    int maxProduct(vector<int>& nums) {
+11        int n = nums.size();
+12        int bestAns = nums[0];
+13
+14        int minBest = 1;
+15        int maxBest = 1;
+16
+17        for(int i = 0; i < n; i++){
+18            int v1 = nums[i];
+19            int v2 = nums[i] * minBest;
+20            int v3 = nums[i] * maxBest;
+21
+22            maxBest = max({v1, v2, v3});
+23            minBest = min({v1, v2, v3});
+24
+25            bestAns = max({bestAns, maxBest, minBest});
+26        }
+27
+28        return bestAns;
+29    }
+30};

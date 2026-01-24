3b09a0a207440853d96e10ed03aa1567b89b69b5
trex-ayush <147419657+trex-ayush@users.noreply.github.com>
2026-01-24 22:07:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Josephus problem/josephus-problem.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int solve(int n,int k){
+        if(n == 1) return 0;
+        return (solve(n - 1, k) + k) % n;
+    }
+  
+    int josephus(int n, int k) {
+        return solve(n,k)+1;
+    }
+};

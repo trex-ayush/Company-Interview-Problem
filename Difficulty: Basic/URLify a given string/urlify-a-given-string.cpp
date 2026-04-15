5a9bb28a220da28956239d31d1a42a1420994236
Ayush Kumar Singh <147419657+trex-ayush@users.noreply.github.com>
2026-04-15 22:26:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/URLify a given string/urlify-a-given-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    string URLify(string &s) {
+        string ans="";
+        
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == ' ') ans+="%20";
+            else ans += s[i];   
+        }
+        
+        return ans;
+    }
+};
